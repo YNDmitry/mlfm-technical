530e56c00e2edbfc9dab661eaa05ce74f78662bd
Dmitry <mailmetelev@gmail.com>
2025-07-03 16:10:32 +0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,22 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  // adapter: node({
-  //   mode: 'standalone'
-  // })
-  server:{
-		port: 3000,
-		host: true
+    output: 'server',
+    site: 'https://example.com',
+    adapter: node({
+    	mode: "standalone"
+    }),
+    server:{
+    		port: 3000,
+    		host: true
+    	},
+     vite:{
+		ssr: {
+			noExternal: ['path-to-regexp'],
+		  },
+		preview: {
+			port: 3000,
+			host: true
+		}
 	}
 });
