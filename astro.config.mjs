94be2055e4e0b10aac9d668e843de4adfe22e505
Dmitry <mailmetelev@gmail.com>
2025-07-03 16:07:16 +0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  })
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+  server:{
+		port: 3000,
+		host: true
+	}
 });
